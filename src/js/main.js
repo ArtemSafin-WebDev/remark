6d37391b959c927fc349d814c8b80945e85a18d9
Artem Safin <artemsafin.webdev@gmail.com>
2021-08-18 11:02:04 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+import polyfills from './polyfills';
+import './lazyload';
+import detectTouch from './detectTouch';
+import setScrollbarWidth from './setScrollbarWidth';
+import validation from './validation';
+import customSelects from './customSelects';
+import masks from './masks';
+import fileUpload from './fileUpload';
+import anchorLinks from './anchorLinks';
+import mediaPlayer from './mediaPlayer';
+import datepicker from './datepicker';
+import modals from './modals';
+import accordions from './accordions';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    polyfills();
+    detectTouch();
+    setScrollbarWidth();
+    validation();
+    customSelects();
+    masks();
+    fileUpload();
+    anchorLinks();
+    accordions();
+    mediaPlayer();
+    modals();
+    datepicker();
+});
+
+window.addEventListener('load', function() {
+    document.body.classList.add('loaded');
+    setTimeout(() => document.body.classList.add('animatable'), 300);
+})
