6d5cdc6d5db9206ba3c590e0047c3719ecfb25f4
Artem Safin <artemsafin.webdev@gmail.com>
2021-09-02 10:37:28 +0300

    История покупок

--- src/js/productCards.js
+++ src/js/productCards.js
@@ -4,49 +4,52 @@ export default function productCards() {
             event.preventDefault();
 
             const productCard = event.target.closest('.product-catalog-card');
-            const input = productCard.querySelector('.product-catalog-card__amount-input');
-            const minusBtn = productCard.querySelector('.product-catalog-card__amount-btn--minus');
+            const input = productCard.querySelector('.js-amount-input');
+            const minusBtn = productCard.querySelector('.js-amount-minus');
 
             minusBtn.disabled = false;
 
             input.value = Number(input.value) + 1;
-
             const changeEvent = new Event('change');
             input.dispatchEvent(changeEvent);
-
             productCard.classList.add('flipped');
         }
 
-        if (event.target.matches('.product-catalog-card__amount-btn--plus') || event.target.closest('.product-catalog-card__amount-btn--plus')) {
+        if (event.target.matches('.js-amount-plus') || event.target.closest('.js-amount-plus')) {
             event.preventDefault();
-            const input = event.target.closest('.product-catalog-card__amount').querySelector('.product-catalog-card__amount-input');
+            const btn = event.target.matches('.js-amount-plus') ? event.target : event.target.closest('.js-amount-plus');
+            const input = btn.parentElement.querySelector('.js-amount-input');
+
+            const minValue = input.hasAttribute('data-min-value') ? Number(input.getAttribute('data-min-value')) : 0;
             
             input.value = Number(input.value) + 1;
 
-            if (Number(input.value) >= 0) {
-                const minusBtn = event.target.closest('.product-catalog-card__amount').querySelector('.product-catalog-card__amount-btn--minus');
+            if (Number(input.value) >= minValue) {
+                const minusBtn = btn.parentElement.querySelector('.js-amount-minus');
                 minusBtn.disabled = false;
             }
             
             const changeEvent = new Event('change');
             input.dispatchEvent(changeEvent);
         }
-        if (event.target.matches('.product-catalog-card__amount-btn--minus') || event.target.closest('.product-catalog-card__amount-btn--minus')) {
+        if (event.target.matches('.js-amount-minus') || event.target.closest('.js-amount-minus')) {
             event.preventDefault();
-            const input = event.target.closest('.product-catalog-card__amount').querySelector('.product-catalog-card__amount-input');
+            const btn = event.target.matches('.js-amount-minus') ? event.target : event.target.closest('.js-amount-minus');
+            const input = btn.parentElement.querySelector('.js-amount-input');
            
             input.value = Number(input.value) - 1;
 
-            if (Number(input.value) <= 0) {
-                input.value = 0;
-                const btn = event.target.matches('.product-catalog-card__amount-btn--minus')
-                ? event.target
-                : event.target.closest('.product-catalog-card__amount-btn--minus');
+            const minValue = input.hasAttribute('data-min-value') ? Number(input.getAttribute('data-min-value')) : 0;
 
+            if (Number(input.value) <= minValue) {
+                input.value = minValue;
+              
                 const card = event.target.closest('.product-catalog-card');
 
-                card.classList.remove('flipped');
-              
+                if (card) {
+                    card.classList.remove('flipped');
+                }
+
                 btn.disabled = true;
             }
             const changeEvent = new Event('change');
