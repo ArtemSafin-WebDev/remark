0388181044fb29bddce4970598d5ad50a01bc9fe
Artem Safin <artemsafin.webdev@gmail.com>
2021-09-16 01:29:56 +0300

    Правки

--- src/assets/js/backend.js
+++ src/assets/js/backend.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-})
+    let footerForm = document.querySelector('#footer-form');
+
+    if (footerForm) {
+        footerForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            if (
+                $(footerForm)
+                    .parsley()
+                    .isValid()
+            ) {
+                footerForm.reset();
+                $(footerForm)
+                    .parsley()
+                    .reset();
+                if (typeof window.openModal === 'function') {
+                    window.openModal('#success');
+                }
+            }
+        });
+    }
+});
