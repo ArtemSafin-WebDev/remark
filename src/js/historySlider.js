3121eb39fba29f898fdfde9137573c69f6c835c8
whiteravil <whiteravil@yandex.ru>
2021-10-13 00:25:56 +0300

    slider for about content

--- src/js/historySlider.js
+++ src/js/historySlider.js
@@ -4,11 +4,15 @@ Swiper.use([Navigation]);
 
 export default function historySlider() {
     const elements = Array.from(document.querySelectorAll('.js-history-slider'));
+    const contents = Array.from(document.querySelectorAll('.js-about-content-slider'));
+
+    let elementsSlider = new Array()
+    let contentsSlider = new Array()
 
     elements.forEach(element => {
         const container = element.querySelector('.swiper-container');
 
-        new Swiper(container, {
+        const slider = new Swiper(container, {
             slidesPerView: 1,
             watchOverflow: true,
             spaceBetween: 0,
@@ -18,5 +22,32 @@ export default function historySlider() {
                 prevEl: element.querySelector('.slider-arrows__btn--prev')
             }
         });
+
+        elementsSlider.push(slider)
+
     });
+
+    contents.forEach(element => {
+        const container = element.querySelector('.swiper-container');
+
+        const slider = new Swiper(container, {
+            slidesPerView: 1,
+            watchOverflow: true,
+            spaceBetween: 30,
+            allowTouchMove: false,
+            speed: 600,
+            watchSlidesVisibility: true
+        });
+
+        contentsSlider.push(slider)
+
+    });
+
+    elementsSlider.forEach((slider, index) => {
+        slider.on('slideChange', function(e) {
+            const i = e.activeIndex
+            contentsSlider[index].slideTo(i)
+        })
+    });
+
 }
